a593f7af77282f91d60f62e3c3f3a0ec557d0c8e
kmj <poodle7192@gmail.com>
2022-08-24 00:25:24 +0900

    push test

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,6 +19,7 @@ public class JpaMain {
            Member findMember = em.find(Member.class, 1L);
            findMember.setName("HelloJPA");
 
+            //commit test
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
