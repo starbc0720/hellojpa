796fd2ce6c161e9f8f0a2836be3b4bf702f9c8c9
김민재 <poodle7192@gmail.com>
2022-08-24 22:35:49 +0900

    영속성관리2 엔티티등록 트랜잭션을 지원~ 전

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,8 +17,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-           Member findMember = em.find(Member.class, 1L);
-           findMember.setName("HelloJPA");
+
+            //영속
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+
+            System.out.println("result = " + (findMember1 == findMember2));
            
             tx.commit();
         } catch (Exception e) {
