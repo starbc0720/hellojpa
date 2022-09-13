5ae1214c678760ec30c595dc7797770d86df59c2
김민재 <poodle7192@gmail.com>
2022-09-13 23:33:37 +0900

    필드와컬럼매핑 중간

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,18 +17,7 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            //영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
-
-            em.clear();
-            System.out.println("=============================1");
-            Member member2 = em.find(Member.class, 150L);
-
-
-            System.out.println("=============================2");
-
+            System.out.println("=============================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,39 +1,31 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@Table(name = "MBR")
 public class Member {
 
     @Id
     private Long id;
-    private String name;
 
-    /*jpa는 기본적으로 기본생성자가 하나 존재해야 함, 내부적으로 reflection을 사용하기 떄문에
-    동적으로 기본 생성자가 하나 존재해야 해서 다음과 같이 선언*/
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
     public Member(){
-    }
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
